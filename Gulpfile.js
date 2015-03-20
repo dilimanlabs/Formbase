0d9bc5eb0d9fd54e39d8f75d49601df3ab5fec2a
Neil John Ramal <ramalneiljohn@gmail.com>
2015-03-20 18:03:33 +0800

    New Changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,31 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    vulcanize = require('gulp-vulcanize');
 
 gulp.task('webserver', function() {
     connect.server({
         livereload: true,
-        root: ['src/']
+        root: ['src/'],
+        port: 9090
     });
 });
 
 gulp.task('livereload', function () {
     gulp.src(['src/q-components/**/*.html', 'src/q-designer/**/*.html', 'src/stylesheet/*.css', 'src/*.html', 'src/preview-components/*.html'])
-        .pipe(watch(['src/gallery-components/**/*.html', 'src/q-components/**/*.html', 'src/q-designer/**/*.html', 'src/stylesheet/*.css', 'src/*.html', 'src/preview-components/*.html']))
+        .pipe(watch(['src/gallery-components/**/*.html', 'src/q-components/**/*.html', 'src/q-designer/**/*.html', 'src/stylesheet/*.css', 'src/*.html', 'src/preview-components/*.html', 'src/formbase/*.html']))
         .pipe(connect.reload());
 });
 
-gulp.task('serve', ['livereload', 'webserver']);
+gulp.task('vulcanize', function () {
+    var DEST_DIR = 'src/formbase';
+    return gulp.src('src/formbase-app.html')
+        .pipe(watch(['src/gallery-components/**/*.html', 'src/q-components/**/*.html', 'src/q-designer/**/*.html', 'src/stylesheet/*.css', 'src/*.html', 'src/preview-components/*.html']))
+        .pipe(vulcanize({
+            dest: DEST_DIR,
+            strip: true
+        }))
+        .pipe(gulp.dest(DEST_DIR));
+});
+
+gulp.task('serve', ['livereload', 'webserver', 'vulcanize']);
